55b02d24e2ae409be1287ccff4dc5cd3db97db6d
8jackson6264 <gnr.axlrose.6.2.1962@gmail.com>
2020-02-26 10:57:35 +0200

    Initial commit

---
+++ src/com/company/GenerateCalendar.java
@@ -0,0 +1,63 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class GenerateCalendar {
+    public static void main(String[] args) {
+        int firstDay = 3;
+        int[][] calendar = new int[12][31];
+        int day;
+        for (int i = 0; i < calendar.length; i++) {
+            for (int j = 0; j < calendar[i].length; j++) {
+                day = (firstDay % 7 != 0 ? firstDay % 7 : 7);
+                if (i == 1 && j == 28){
+                    calendar[i][j] = day;
+                    break;
+                } else if (i<7 && i%2 == 1 && j==29){
+                    calendar[i][j] = day;
+                    break;
+                } else if (i>6 && i%2 == 0 && j == 29){
+                    calendar[i][j] = day;
+                    break;
+                }
+                calendar[i][j] = day;
+                firstDay++;
+            }
+        }
+
+        for (int i = 0; i < calendar.length; i++) {
+            System.out.println(Arrays.toString(calendar[i]));
+        }
+
+        for (int i = 0; i < calendar.length; i++) {
+            for (int j = 0; j < calendar[i].length; j++) {
+                switch (calendar[i][j]) {
+                    case 1:
+                        System.out.print("Понеделник ");
+                        break;
+                    case 2:
+                        System.out.print("Вторник ");
+                        break;
+                    case 3:
+                        System.out.print("Сряда ");
+                        break;
+                    case 4:
+                        System.out.print("Четвъртък ");
+                        break;
+                    case 5:
+                        System.out.print("Петък ");
+                        break;
+                    case 6:
+                        System.out.print("Събота ");
+                        break;
+                    case 7:
+                        System.out.print("Неделя ");
+                        break;
+                }
+                if (j%7 == 6) System.out.println();
+            }
+            System.out.println("\n");
+        }
+
+    }
+}
